4031dce3b8a2e1345f270940c6f060be219b8392
Shawn <sbenn9210@gmail.com>
2025-12-18 09:05:16 -0600

    Fix migrations

--- backend/agent/alembic/versions/dfb60e1f20bf_add_pgvector_and_document_tables.py
+++ backend/agent/alembic/versions/dfb60e1f20bf_add_pgvector_and_document_tables.py
@@ -21,6 +21,7 @@ depends_on: Union[str, Sequence[str], None] = None
 
 def upgrade() -> None:
     """Upgrade schema."""
+    op.execute("CREATE SCHEMA IF NOT EXISTS extensions;")
     op.execute("CREATE EXTENSION IF NOT EXISTS vector WITH SCHEMA extensions;")
 
     op.create_table(
